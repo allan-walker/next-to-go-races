1cf0372c1e6a16d7f0a94ee1bb4d7887488dc63b
Allan Walker <al@numeral.nz>
2025-10-14 20:50:13 +1100

    Add simple UI components to start testing API -> state workflow.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import RaceList from '@/components/RaceList.vue'
+import { useRacesStore } from '@/stores/races.store'
+
+const store = useRacesStore()
+
+onMounted(async () => {
+  await store.refresh()
+})
+</script>
 
 <template>
   <header>
     <h1 class="text-4xl font-semibold text-white">Next to Go</h1>
   </header>
 
-  <main></main>
+  <main>
+    <RaceList />
+  </main>
 </template>
---
+++ src/components/RaceList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRacesStore } from '@/stores/races.store'
+
+const store = useRacesStore()
+</script>
+
+<template>
+  <ul class="mt-4 grid gap-3">
+    <!-- @TODO: Display this only when there are no results -->
+    <li class="text-gray-500">No upcoming races for selected categories.</li>
+  </ul>
+</template>
