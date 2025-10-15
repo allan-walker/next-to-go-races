d0f172174182ffbbe5093c3d362fd74277b7e2ed
Allan Walker <al@numeral.nz>
2025-10-15 11:14:46 +1100

    Added a brand-styled site header.

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BrandWordmark from '@/assets/brand-wordmark-onbrand.svg'
+</script>
+
+<template>
+  <header class="bg-brand text-white flex items-center justify-start px-6 py-3 shadow-soft">
+    <img :src="BrandWordmark" alt="Brand logo" class="h-6 md:h-8" />
+  </header>
+</template>
